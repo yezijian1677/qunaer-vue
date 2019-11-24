4f2055f791fe8f9dec684e2bcd976afa70431817
yezijian <1677390657@qq.com>
2019-11-24 21:17:24 +0800

    add select city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <city-header/>
-        <city-search/>
+        <city-search :cities="cities"/>
         <city-list :cities="cities" :hotCities="hotCities" :letter="letter"/>
         <city-aphabet :cities="cities" @change="handleLetterChange"/>
     </div>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -8,7 +8,7 @@
             输入城市/游玩主题
         </div>
         <router-link to="/city">
-            <div class="header-right">{{this.city}}
+            <div class="header-right">北京
                 <span class="iconfont arrow-icon">&#xe64a;</span>
             </div>
         </router-link>
